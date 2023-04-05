185173621ebfbc98490f996c5d4f5c4c87c33f6b
dranishnikov.n <dranishnikov.n@47tech.ru>
2023-04-05 05:59:19 +0300

    fix code2

--- src/main/java/ru/practicum/shareit/user/exceptions/UserNotFoundException.java
+++ src/main/java/ru/practicum/shareit/user/exceptions/UserNotFoundException.java
@@ -1,7 +1,8 @@
 package ru.practicum.shareit.user.exceptions;
 
-public class UserNotFoundException extends Exception{
-    public UserNotFoundException() {}
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException() {
+    }
 
     public UserNotFoundException(String message) {
         super(message);
