19392ac494aa80f6746ac1d56564aa06b536a26f
dranishnikov.n <dranishnikov.n@47tech.ru>
2023-04-05 05:46:55 +0300

    fix code

--- src/main/java/ru/practicum/shareit/item/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -4,7 +4,7 @@ import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
 public class ItemMapper {
-    public static ItemDto toItemDto (Item item) {
+    public static ItemDto toItemDto(Item item) {
         return new ItemDto(
                 item.getName(),
                 item.getDescription(),
@@ -13,7 +13,7 @@ public class ItemMapper {
         );
     }
 
-    public static Item toItem (ItemDto itemDto) {
+    public static Item toItem(ItemDto itemDto) {
         return new Item(
                 itemDto.getName(),
                 itemDto.getDescription(),
--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -17,7 +17,6 @@ public class UserServiceImpl implements UserService {
         this.userStorage = userStorage;
     }
 
-
     @Override
     public List<User> getAllUsers() {
         return (List<User>) userStorage.getAllUsers();
