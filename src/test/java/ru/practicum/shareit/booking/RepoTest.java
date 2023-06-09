508e4d39057012ede704d555f1194534522d104f
dranishnikov.n <dranishnikov.n@47tech.ru>
2023-06-09 18:22:24 +0700

    add some tests

---
+++ src/test/java/ru/practicum/shareit/booking/RepoTest.java
@@ -0,0 +1,157 @@
+package ru.practicum.shareit.booking;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.booking.projection.BookingShort;
+import ru.practicum.shareit.booking.repo.BookingRepo;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.item.repo.ItemRepo;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.repo.RequestRepo;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.repo.UserRepo;
+
+import java.time.LocalDateTime;
+
+import static ru.practicum.shareit.booking.model.BookingStatus.APPROVED;
+import static ru.practicum.shareit.booking.model.BookingStatus.WAITING;
+
+@DataJpaTest
+@AutoConfigureTestDatabase
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RepoTest {
+    @Autowired
+    BookingRepo bookingRepo;
+    @Autowired
+    TestEntityManager tem;
+    @Autowired
+    ItemRepo itemRepo;
+    @Autowired
+    UserRepo userRepo;
+    @Autowired
+    RequestRepo requestRepo;
+    Booking booking;
+    User booker;
+    User owner;
+    Item item;
+    final Pageable request = PageRequest.of(0, 10);
+
+    private User setUser(String name, String email) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        return user;
+    }
+
+    private ItemRequest setRequest(User user) {
+        ItemRequest request = new ItemRequest();
+        request.setDescription("Request for test item");
+        request.setUser(user);
+        return request;
+    }
+
+    private Item setItem(User user, ItemRequest request) {
+        Item item = new Item();
+        item.setName("Test item");
+        item.setDescription("Test item description");
+        item.setOwner(user.getId());
+        item.setAvailable(true);
+        item.setRequest(request);
+        item.setComments(null);
+        return item;
+    }
+
+    private Booking setBooking(User user, Item item) {
+        Booking booking = new Booking();
+        booking.setStart(LocalDateTime.now());
+        booking.setEnd(LocalDateTime.now().plusDays(1));
+        booking.setStatus(BookingStatus.WAITING);
+        booking.setItem(item);
+        booking.setBooker(user);
+        return booking;
+    }
+
+    @BeforeEach
+    void setUp() {
+        owner = setUser("Test user", "user@mail.com");
+        booker = setUser("Test owner", "owner@mail.com");
+        userRepo.save(owner);
+        userRepo.save(booker);
+        ItemRequest request = setRequest(owner);
+        requestRepo.save(request);
+        item = setItem(owner, request);
+        itemRepo.save(item);
+        booking = setBooking(booker, item);
+        bookingRepo.save(booking);
+    }
+
+    @Test
+    void testUpdateStatus() {
+        bookingRepo.updateStatus(booking.getId(), APPROVED);
+        Assertions.assertEquals(APPROVED, bookingRepo.findById(booking.getId()).orElseThrow().getStatus());
+    }
+
+    @Test
+    void testFindBookingShortByBookingId() {
+        BookingShort bookingShort = bookingRepo.findBookingShortByBookingId(booking.getId());
+        Assertions.assertEquals(booking.getId(), bookingShort.getId());
+        Assertions.assertEquals(booking.getStatus(), bookingShort.getStatus());
+        Assertions.assertEquals(booker.getId(), bookingShort.getBooker().getId());
+        Assertions.assertEquals(item.getId(), bookingShort.getItem().getId());
+    }
+
+    @Test
+    void testFindAllByBookerIdOrderByStartDesc() {
+        Page<BookingShort> bookings = bookingRepo.findAllByBookerIdOrderByStartDesc(booker.getId(), request);
+        Assertions.assertEquals(1, bookings.getContent().size());
+        Assertions.assertEquals(booking.getId(), bookings.getContent().get(0).getId());
+        Assertions.assertEquals(booking.getStatus(), bookings.getContent().get(0).getStatus());
+        Assertions.assertEquals(booker.getId(), bookings.getContent().get(0).getBooker().getId());
+        Assertions.assertEquals(item.getId(), bookings.getContent().get(0).getItem().getId());
+    }
+
+    @Test
+    void testFindAllByBookerIdAndStatusOrderByStartDesc() {
+        Page<BookingShort> bookings = bookingRepo
+                .findAllByBookerIdAndStatusOrderByStartDesc(booker.getId(), WAITING, request);
+        Assertions.assertEquals(1, bookings.getContent().size());
+        Assertions.assertEquals(booking.getId(), bookings.getContent().get(0).getId());
+        Assertions.assertEquals(booking.getStatus(), bookings.getContent().get(0).getStatus());
+        Assertions.assertEquals(booker.getId(), bookings.getContent().get(0).getBooker().getId());
+        Assertions.assertEquals(item.getId(), bookings.getContent().get(0).getItem().getId());
+    }
+
+    @Test
+    void testFindAllByOwnerIdOrderByStartDesc() {
+        Page<BookingShort> bookings = bookingRepo
+                .findAllByOwnerIdOrderByStartDesc(owner.getId(), request);
+        Assertions.assertEquals(1, bookings.getContent().size());
+        Assertions.assertEquals(booking.getId(), bookings.getContent().get(0).getId());
+        Assertions.assertEquals(booking.getStatus(), bookings.getContent().get(0).getStatus());
+        Assertions.assertEquals(booker.getId(), bookings.getContent().get(0).getBooker().getId());
+        Assertions.assertEquals(item.getId(), bookings.getContent().get(0).getItem().getId());
+    }
+
+    @Test
+    void testFindAllByOwnerIdAndStatusOrderByStartDesc() {
+        Page<BookingShort> bookings = bookingRepo
+                .findAllByOwnerIdAndStatusOrderByStartDesc(owner.getId(), WAITING, request);
+        Assertions.assertEquals(1, bookings.getContent().size());
+        Assertions.assertEquals(booking.getId(), bookings.getContent().get(0).getId());
+        Assertions.assertEquals(booking.getStatus(), bookings.getContent().get(0).getStatus());
+        Assertions.assertEquals(booker.getId(), bookings.getContent().get(0).getBooker().getId());
+        Assertions.assertEquals(item.getId(), bookings.getContent().get(0).getItem().getId());
+    }
+}
---
+++ src/test/java/ru/practicum/shareit/booking/ServiceIntegrationalTest.java
@@ -0,0 +1,150 @@
+package ru.practicum.shareit.booking;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.practicum.shareit.booking.dto.BookingRequestDto;
+import ru.practicum.shareit.booking.dto.BookingResponseDto;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.exception.BookerIsOwnerException;
+import ru.practicum.shareit.exception.BookingNotFoundException;
+import ru.practicum.shareit.exception.ItemUnavailableException;
+import ru.practicum.shareit.exception.UpdateStatusAfterApprovalException;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.exceptions.BadUserForItem;
+import ru.practicum.shareit.item.exceptions.InvalidItemAvailable;
+import ru.practicum.shareit.item.service.ItemService;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.mapper.ItemRequestMapper;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.request.service.RequestService;
+import ru.practicum.shareit.user.exceptions.UserDuplicateException;
+import ru.practicum.shareit.user.service.UserService;
+import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.exception.WrongDateException;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureTestDatabase
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ServiceIntegrationalTest {
+    @Autowired
+    ItemService itemService;
+    @Autowired
+    UserService userService;
+    @Autowired
+    RequestService requestService;
+    @Autowired
+    BookingService bookingService;
+    User ownerTest;
+    User bookerTest;
+    ItemRequestDto itemRequestDtoTest;
+    ItemDto itemDtoTest;
+    BookingResponseDto bookingResponseDtoTest;
+
+    private User setUser(String name, String email) {
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        return user;
+    }
+
+    private ItemRequestDto setRequestDto() {
+        return ItemRequestDto.builder()
+                .description("Request")
+                .build();
+    }
+
+    private ItemDto setItemDto(ItemRequestDto requestDto) {
+        ItemRequest itemRequest = new ItemRequest();
+        itemRequest.setId(requestDto.getId());
+        itemRequest.setDescription(requestDto.getDescription());
+        return ItemDto.builder()
+                .id(Math.toIntExact(requestDto.getId()))
+                .name("Item name")
+                .description("Item description")
+                .available(true)
+                .request(itemRequest)
+                .build();
+    }
+
+    private BookingRequestDto setFutureBookingDto(ItemDto itemDto) {
+        BookingRequestDto bookingDto = new BookingRequestDto();
+        LocalDateTime nextStart = LocalDateTime.now().plusDays(1);
+        LocalDateTime nextEnd = nextStart.plusDays(2);
+        bookingDto.setStart(nextStart);
+        bookingDto.setEnd(nextEnd);
+        bookingDto.setItemId(itemDto.getId());
+        return bookingDto;
+    }
+
+    @BeforeEach
+    void setUp() throws UserDuplicateException, InvalidItemAvailable {
+        User owner = setUser("Test owner", "owner@mail.com");
+        ownerTest = userService.addUser(owner);
+        User booker = setUser("Test booker", "booker@mail.com");
+        bookerTest = userService.addUser(booker);
+        ItemRequestDto requestDto = setRequestDto();
+        itemRequestDtoTest = requestService.addItemRequest(bookerTest.getId(), requestDto);
+        ItemDto itemDto = setItemDto(itemRequestDtoTest);
+        itemDtoTest = itemService.addItem(itemDto, ownerTest.getId());
+        BookingRequestDto bookingDto = setFutureBookingDto(itemDtoTest);
+        bookingResponseDtoTest = bookingService.addBooking(bookerTest.getId(), bookingDto);
+    }
+
+    @Test
+    void testGetBookingById() {
+        assertThat(bookingService.getBookingById(ownerTest.getId(), bookingResponseDtoTest.getId()).getStatus(),
+                equalTo(BookingStatus.WAITING));
+        Assertions.assertThrows(BookingNotFoundException.class,
+                () -> bookingService.getBookingById(ownerTest.getId(), 99L));
+    }
+
+    @Test
+    void testRejectedStatus() {
+        bookingService.setBookingStatus(ownerTest.getId(), bookingResponseDtoTest.getId(), false);
+        assertThat(bookingService.getBookingById(ownerTest.getId(), itemRequestDtoTest.getId()).getStatus(),
+                equalTo(BookingStatus.REJECTED));
+        Assertions.assertThrows(UpdateStatusAfterApprovalException.class,
+                () -> bookingService.setBookingStatus(ownerTest.getId(), bookingResponseDtoTest.getId(), true));
+    }
+
+    @Test
+    void testAddBookingByOwnerFailed() {
+        Assertions.assertThrows(BookerIsOwnerException.class,
+                () -> bookingService.addBooking(ownerTest.getId(), setFutureBookingDto(itemDtoTest)));
+    }
+
+    @Test
+    void testAddBookingWithIncorrectDate() {
+        BookingRequestDto bookingRequestDto = setFutureBookingDto(itemDtoTest);
+        bookingRequestDto.setStart(LocalDateTime.now());
+        bookingRequestDto.setEnd(LocalDateTime.now());
+        Assertions.assertThrows(WrongDateException.class,
+                () -> bookingService.addBooking(bookerTest.getId(), bookingRequestDto));
+    }
+
+    @Test
+    void testAddBookingUnavailableItemFailed() throws BadUserForItem {
+        ItemDto itemDto = itemDtoTest;
+        itemDto.setAvailable(false);
+        itemService.updateItem(ownerTest.getId(), itemDto, ownerTest.getId());
+        Assertions.assertThrows(ItemUnavailableException.class,
+                () -> bookingService.addBooking(bookerTest.getId(), setFutureBookingDto(itemDto)));
+    }
+}
