d1e781f630fc73d5da12de3567a8258d5d6a96ca
dranishnikov.n <dranishnikov.n@47tech.ru>
2023-06-16 16:47:14 +0700

    some changes

--- shareIt-server/src/main/java/ru/practicum/server/item/service/ItemServiceImpl.java
+++ shareIt-server/src/main/java/ru/practicum/server/item/service/ItemServiceImpl.java
@@ -25,7 +25,6 @@ import ru.practicum.server.user.repo.UserRepo;
 import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
